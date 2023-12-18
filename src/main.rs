4b9c0c897a4f289ca08854b5139ef9e801b8fee6
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-12-18 23:06:27 +0100

    Setup project

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::net::{TcpListener, TcpStream};
+
+type Store = Arc<Mutex<HashMap<String, String>>>;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let store = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        let store = store.clone();
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(socket, store).await {
+                println!("Error: {}", e);
+            }
+        });
+    }
+}
+
+async fn handle_connection(
+    mut stream: TcpStream,
+    store: Store,
+) -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
