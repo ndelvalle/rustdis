276e25f777592a8dac730ca8260585092b43fa1f
gillchristian <gillchristiang@gmail.com>
2024-04-02 12:50:34 +0200

    Exists command
    Also remove intermediate step in try_from parser to command

--- src/commands/exists.rs
+++ src/commands/exists.rs
@@ -2,6 +2,7 @@ use std::sync::{Arc, Mutex};
 
 use crate::commands::executable::Executable;
 use crate::commands::CommandParser;
+use crate::commands::CommandParserError;
 use crate::frame::Frame;
 use crate::store::Store;
 use crate::Error;
@@ -12,8 +13,15 @@ pub struct Exists {
 }
 
 impl Executable for Exists {
-    fn exec(self, _store: Arc<Mutex<Store>>) -> Result<Frame, Error> {
-        Ok(Frame::Simple("OK".to_string()))
+    fn exec(self, store: Arc<Mutex<Store>>) -> Result<Frame, Error> {
+        let mut count = 0;
+        let store = store.lock().unwrap();
+        for key in self.keys {
+            if store.exists(&key) {
+                count += 1;
+            }
+        }
+        Ok(Frame::Integer(count))
     }
 }
 
@@ -21,8 +29,87 @@ impl TryFrom<&mut CommandParser> for Exists {
     type Error = Error;
 
     fn try_from(parser: &mut CommandParser) -> Result<Self, Self::Error> {
-        let key = parser.next_string()?;
-        println!("key: {}", key);
-        Ok(Self { keys: vec![key] })
+        let mut keys = vec![];
+
+        loop {
+            match parser.next_string() {
+                Ok(key) => keys.push(key),
+                Err(CommandParserError::EndOfStream) if !keys.is_empty() => {
+                    break;
+                }
+                Err(err) => return Err(err.into()),
+            }
+        }
+
+        Ok(Self { keys })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+
+    use crate::commands::Command;
+
+    use super::*;
+
+    #[test]
+    fn multiple_keys() {
+        let frame = Frame::Array(vec![
+            Frame::Bulk(Bytes::from("EXISTS")),
+            Frame::Bulk(Bytes::from("foo")),
+            Frame::Bulk(Bytes::from("bar")),
+            Frame::Bulk(Bytes::from("baz")),
+        ]);
+        let cmd = Command::try_from(frame).unwrap();
+        assert_eq!(
+            cmd,
+            Command::Exists(Exists {
+                keys: vec!["foo".to_string(), "bar".to_string(), "baz".to_string()]
+            })
+        );
+    }
+
+    #[test]
+    fn single_key() {
+        let frame = Frame::Array(vec![
+            Frame::Bulk(Bytes::from("EXISTS")),
+            Frame::Bulk(Bytes::from("foo")),
+        ]);
+        let cmd = Command::try_from(frame).unwrap();
+        assert_eq!(
+            cmd,
+            Command::Exists(Exists {
+                keys: vec!["foo".to_string()]
+            })
+        );
+    }
+
+    #[test]
+    fn zero_keys() {
+        let frame = Frame::Array(vec![Frame::Bulk(Bytes::from("EXISTS"))]);
+        let err = Command::try_from(frame).err().unwrap();
+        let err = err.downcast_ref::<CommandParserError>().unwrap();
+
+        assert_eq!(*err, CommandParserError::EndOfStream);
+    }
+
+    #[test]
+    fn invalid_frame() {
+        let frame = Frame::Array(vec![
+            Frame::Bulk(Bytes::from("EXISTS")),
+            Frame::Integer(42),
+            Frame::Bulk(Bytes::from("foo")),
+        ]);
+        let err = Command::try_from(frame).err().unwrap();
+        let err = err.downcast_ref::<CommandParserError>().unwrap();
+
+        assert_eq!(
+            *err,
+            CommandParserError::InvalidFrame {
+                expected: "simple or bulk string".to_string(),
+                actual: Frame::Integer(42)
+            }
+        );
     }
 }
