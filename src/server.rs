f33c54459181bf7323ad08d9ca84cb25e51eef82
Nicolas del Valle <nicolas.delvalle@gmail.com>
2024-04-20 18:20:18 +0200

    Implement basic tracing

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
 use std::sync::{Arc, Mutex};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
+use tracing::info;
 
 use crate::commands::executable::Executable;
 use crate::commands::Command;
@@ -9,12 +10,18 @@ use crate::store::Store;
 use crate::Error;
 
 pub async fn run() -> Result<(), Error> {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
     let store = Arc::new(Mutex::new(Store::new()));
+
+    info!("Redis server listening on {}", listener.local_addr()?);
 
     loop {
         let (socket, _) = listener.accept().await?;
         let store = store.clone();
+        info!("Accepted connection from {:?}", socket.peer_addr()?);
 
         tokio::spawn(async move {
             if let Err(e) = handle_connection(socket, store).await {
@@ -28,6 +35,7 @@ async fn handle_connection(stream: TcpStream, store: Arc<Mutex<Store>>) -> Resul
     let mut con = Connection::new(stream);
 
     while let Some(frame) = con.read_frame().await? {
+        info!("Received frame: {:?}", frame);
         let cmd = Command::try_from(frame)?;
         let res = cmd.exec(store.clone())?;
         let res: Vec<u8> = res.into();
