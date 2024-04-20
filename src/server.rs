022320c7022fb3e393a89f83bc399b15d3eff0c2
Nicolas del Valle <nicolas.delvalle@gmail.com>
2024-04-20 21:47:24 +0200

    Add tracing spans

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
+use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
-use tracing::info;
+use tracing::{info, instrument};
 
 use crate::commands::executable::Executable;
 use crate::commands::Command;
@@ -9,38 +10,47 @@ use crate::connection::Connection;
 use crate::store::Store;
 use crate::Error;
 
+const PORT: u16 = 6379;
+
 pub async fn run() -> Result<(), Error> {
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
 
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let listener = TcpListener::bind(("127.0.0.1", PORT)).await?;
     let store = Arc::new(Mutex::new(Store::new()));
 
     info!("Redis server listening on {}", listener.local_addr()?);
 
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, client_address) = listener.accept().await?;
         let store = store.clone();
-        info!("Accepted connection from {:?}", socket.peer_addr()?);
+        info!("Accepted connection from {:?}", client_address);
 
         tokio::spawn(async move {
-            if let Err(e) = handle_connection(socket, store).await {
+            if let Err(e) = handle_connection(socket, client_address, store).await {
                 println!("Error: {}", e);
             }
         });
     }
 }
 
-async fn handle_connection(stream: TcpStream, store: Arc<Mutex<Store>>) -> Result<(), Error> {
-    let mut con = Connection::new(stream);
+#[instrument(name = "connection", skip(stream, store), fields(connection_id))]
+async fn handle_connection(
+    stream: TcpStream,
+    client_address: SocketAddr,
+    store: Arc<Mutex<Store>>,
+) -> Result<(), Error> {
+    let mut conn = Connection::new(stream, client_address);
 
-    while let Some(frame) = con.read_frame().await? {
+    tracing::Span::current().record("connection_id", conn.id.to_string());
+
+    while let Some(frame) = conn.read_frame().await? {
         info!("Received frame: {:?}", frame);
         let cmd = Command::try_from(frame)?;
         let res = cmd.exec(store.clone())?;
         let res: Vec<u8> = res.into();
 
-        con.writer.write_all(&res).await?;
+        conn.writer.write_all(&res).await?;
     }
 
     Ok(())
