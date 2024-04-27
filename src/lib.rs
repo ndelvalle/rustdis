137723d3374af88b0b9a970fc3cc2a6c77c1572c
gillchristian <gillchristiang@gmail.com>
2024-04-27 22:58:09 +0200

    cmds(string): lcs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod connection;
 pub mod frame;
 pub mod server;
 pub mod store;
+pub mod utils;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
