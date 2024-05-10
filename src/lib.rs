6763fc6a467930c4b34727c7314016b81646e6de
Nicolas del Valle <nicolas.delvalle@gmail.com>
2024-05-10 18:21:53 +0700

    Add documentation to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! Rustdis is a partial Redis server implementation intended purely for educational purposes.
+//!
+//! The primary goal of rustdis is to offer a straightforward and comprehensible implementation,
+//! with no optimization techniques to ensure the code remains accessible and easy to understand.
+//! As of now, rustdis focuses exclusively on implementing Redis' String data type and its
+//! associated methods. You can find more about Redis strings here: [Redis
+//! Strings](https://redis.io/docs/data-types/strings/).
+//!
+//! # Architecture
+//!
+//! * `server`: Redis server module. Provides a run function that initiates the server, enabling it
+//! to begin handling incoming connections from Redis clients. It manages client requests, executes
+//! Redis commands, and handles connection lifecycles.
+//!
+//! * `connection`: The Connection module manages a TCP connection for a Redis client. It separates
+//! the TCP stream into readable and writable components to facilitate data consumption and
+//! transmission. The server uses this connection module to read data from the TCP connection.
+//! Additionally, the connection module uses the codec module to convert raw TCP bytes into
+//! comprehensible data structures (Frames).
+
 pub mod codec;
 pub mod commands;
 pub mod connection;
